e6524a6855e9e7bce2177e45f8b08541eaef2a81
Kevin Hwang <hjs6877@gmail.com>
2016-10-20 09:57:33 +0900

    '.'

--- InterView/src/main/java/com/siksco/array/Matrix.java
+++ InterView/src/main/java/com/siksco/array/Matrix.java
@@ -6,23 +6,26 @@ package com.siksco.array;
 public class Matrix {
 
     public int[][] changeAllRowColWithZeroToZero(int[][] matrix){
-        boolean isEnd = false;
-        for(int i=0; i < matrix.length; i++){
-            if(isEnd) break;
+        boolean[] row = new boolean[matrix.length];
+        boolean[] col = new boolean[matrix[0].length];
+
+        for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
                 if(matrix[i][j] == 0){
-                    for(int m = 0; m < matrix[i].length; m++){
-                        matrix[i][m] = 0;
-                    }
-
-                    for(int n = 0; n < matrix.length; n++){
-                        matrix[n][j] = 0;
-                    }
-                    isEnd = true;
-                    break;
+                    row[i] = true;
+                    col[i] = true;
                 }
             }
         }
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
         return matrix;
     }
 }
